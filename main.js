9380f38937bf3624b0f4aeb7e02a98ce830da83d
Dexter Lesaca <emobile@gmail.com>
2020-05-14 16:54:19 -0700

    More credentials to env file

--- main.js
+++ main.js
@@ -3,11 +3,10 @@ const graphqlHTTP = require('express-graphql');
 const port =  8080;
 const mongoose = require('mongoose');
 const schema = require('./schema/schema');
-const MONGO_URI = 'mongodb+srv://wgadmin:x846TCmO8JIo41cg@warguidecluster-cqwmo.mongodb.net/test?retryWrites=true&w=majority';
 
 const app = express();
 
-mongoose.connect(MONGO_URI, { 
+mongoose.connect(process.env.MONGO_URI, { 
   useNewUrlParser: true,
   useUnifiedTopology: true 
 })
